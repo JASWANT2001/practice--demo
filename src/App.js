efec3292987d62655e15de75922e158c268a1c45
JASWANT2001 <k.jaswant2305@gmail.com>
2024-01-04 21:39:21 +0530

    Content Added

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className='bg'>
+    
+    <div className='cont'>
       <div className='login'>
+
        <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email address</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
           <div className='color' id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
+
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Password</label>
           <input type="password" class="form-control" id="exampleInputPassword1"></input>
         </div>
+
         <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </div>
+  
 
   );
 }
